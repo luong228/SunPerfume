52abcbd1c09a127330aa42c1c89e7615ad613a4d
luong228 <90311663+luong228@users.noreply.github.com>
2023-02-05 10:28:56 +0700

    Init Models

---
+++ SunPerfume.Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunPerfume.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Note { get; set; }
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ SunPerfume.Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunPerfume.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Price { get; set; }
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+        [Required]
+        [DisplayName("Category")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+        [Required]
+        [DisplayName("Brand")]
+        public int BrandId { get; set; }
+        [ForeignKey("BrandId")]
+        [ValidateNever]
+        public Brand Brand { get; set; }
+        
+    }
+}
