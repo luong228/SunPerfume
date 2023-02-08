cf73831c657e27b1ad80df9d57b1fd7a24b07f16
luong228 <90311663+luong228@users.noreply.github.com>
2023-02-08 23:09:43 +0700

    CRUD for Product

--- SunPerfume.Models/ViewModels/ProductVM.cs
+++ SunPerfume.Models/ViewModels/ProductVM.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,19 @@ namespace SunPerfume.Models.ViewModels
 {
     public class ProductVM
     {
+        //Customer Area
+        [ValidateNever]
         public IEnumerable<Product> ProductList { get; set; }
+        [ValidateNever]
         public Category Category { get; set; }
+        [ValidateNever]
         public Brand Brand { get; set; }
+        // Admin Area
+        public Product Product { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CategorySelectList { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> BrandSelectList { get; set; }
 
     }
 }
--- SunPerfume/Areas/Admin/Controllers/ProductController.cs
+++ SunPerfume/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Hosting;
 using SunPerfume.DataAccess.Repository.IRepository;
+using SunPerfume.Models;
+using SunPerfume.Models.ViewModels;
 
 namespace SunPerfume.Area.Admin.Controllers
 {
@@ -7,13 +11,117 @@ namespace SunPerfume.Area.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public ProductVM ProductVM { get; set; }
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
-            _unitOfWork= unitOfWork;
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> listProduct = _unitOfWork.ProductRepository.GetAll();
+            return View(listProduct);
         }
+        public IActionResult Upsert(string? id)
+        {
+            ProductVM ProductVM = new()
+            {
+                Product = new(),
+                CategorySelectList = _unitOfWork.CategoryRepository.GetAll().Select(
+                    u => new SelectListItem
+                    {
+                        Text = u.Name,
+                        Value = u.CategoryId
+                    }),
+                BrandSelectList = _unitOfWork.BrandRepository.GetAll().Select(
+                    u => new SelectListItem
+                    {
+                        Text = u.Name,
+                        Value = u.BrandId
+                    }),
+            };
+            if (id == null)
+            {
+                return View(ProductVM);
+            }
+            else
+            {
+                ProductVM.Product = _unitOfWork.ProductRepository.GetFirstOrDefault(u => u.ProductId == id);
+                return View(ProductVM);
+            }
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM obj, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(wwwRootPath, @"images\products");
+                    var extension = Path.GetExtension(file.FileName);
+
+                    // Delete old image if exist
+                    if (obj.Product.ImageUrl != null)
+                    {
+                        var test = obj.Product.ImageUrl.TrimStart('\\');
+                        var oldImagePath = wwwRootPath + "\\images\\";
+                        oldImagePath = Path.Combine(oldImagePath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        //Copy content from file to fileStream that already created
+                        file.CopyTo(fileStreams);
+                    }
+                    obj.Product.ImageUrl = @"\products\" + fileName + extension;
+                }
+                if (obj.Product.ProductId == "new")
+                {
+                    _unitOfWork.ProductRepository.Add(obj.Product);
+                    TempData["success"] = "Product created successfully";
+
+                }
+                else
+                {
+                    _unitOfWork.ProductRepository.Update(obj.Product);
+                    TempData["success"] = "Product updated successfully";
+                }
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+        #region API CALL
+        //POST
+        [HttpDelete]
+        public IActionResult Delete(string? id)
+        {
+            var obj = _unitOfWork.ProductRepository.GetFirstOrDefault(u => u.ProductId == id);
+            if (obj == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            var oldImagePath = wwwRootPath + "\\images\\";
+            oldImagePath = Path.Combine(oldImagePath, obj.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+            _unitOfWork.ProductRepository.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Product deleted successfully" });
+        }
+        #endregion
     }
 }
