40e74244739505b91ef7c7114f00e3d8c3cf4968
luong228 <90311663+luong228@users.noreply.github.com>
2023-02-09 11:23:10 +0700

    Add Cart & Summary Cart Page

--- SunPerfume/Areas/Customer/Controllers/CartController.cs
+++ SunPerfume/Areas/Customer/Controllers/CartController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using SunPerfume.DataAccess.Repository.IRepository;
+using SunPerfume.Models.ViewModels;
+using System.Security.Claims;
 
 namespace SunPerfumeWeb.Areas.Customer.Controllers
 {
@@ -12,6 +14,8 @@ namespace SunPerfumeWeb.Areas.Customer.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IEmailSender _emailSender;
+        [BindProperty]
+        CartVM CartVM { get; set; }
         public CartController(IUnitOfWork unitOfWork, IEmailSender emailSender)
         {
             _unitOfWork = unitOfWork;
@@ -20,7 +24,47 @@ namespace SunPerfumeWeb.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity?)User.Identity;
+            var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+
+            CartVM = new CartVM()
+            {
+                ListCart = _unitOfWork.CartRepository.GetAll(u => u.ApplicationUserId == claim.Value,
+                 includeProperties: "Product"),
+                OrderHeader = new()
+            };
+            foreach (var cart in CartVM.ListCart)
+            {
+                CartVM.OrderHeader.OrderTotal += cart.Product.Price * cart.Count;
+            }
+            return View(CartVM);
         }
-    }
+		public IActionResult Summary()
+		{
+			var claimsIdentity = (ClaimsIdentity?)User.Identity;
+			var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+
+			CartVM = new CartVM()
+			{
+				ListCart = _unitOfWork.CartRepository.GetAll(u => u.ApplicationUserId == claim.Value,
+				 includeProperties: "Product"),
+				OrderHeader = new()
+			};
+			CartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUserRepository.GetFirstOrDefault(
+				u => u.Id == claim.Value
+				);
+			CartVM.OrderHeader.Name = CartVM.OrderHeader.ApplicationUser.Name;
+			CartVM.OrderHeader.PhoneNumber = CartVM.OrderHeader.ApplicationUser.PhoneNumber;
+			CartVM.OrderHeader.StreetAddress = CartVM.OrderHeader.ApplicationUser.StreetAddress;
+			CartVM.OrderHeader.City = CartVM.OrderHeader.ApplicationUser.City;
+			CartVM.OrderHeader.State = CartVM.OrderHeader.ApplicationUser.State;
+			CartVM.OrderHeader.PostalCode = CartVM.OrderHeader.ApplicationUser.PostalCode;
+
+			foreach (var cart in CartVM.ListCart)
+			{
+				CartVM.OrderHeader.OrderTotal += cart.Product.Price * cart.Count;
+			}
+			return View(CartVM);
+		}
+	}
 }
